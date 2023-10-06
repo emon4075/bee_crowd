0bb21c92c47955cde4c8ec238fc8a20b78594f8b
emon4075 <mdabdullahemon4075@gmail.com>
2023-10-06 18:47:28 +0600

    6/10/2023

---
+++ Rest1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int A, B;
+    cin >> A >> B;
+    cout << A % B << endl;
+    return 0;
+}
